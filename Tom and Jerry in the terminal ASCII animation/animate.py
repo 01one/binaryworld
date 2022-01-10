747cad7a3f357c3ab8446005cd93c093f1caaea6
01one <84787013+01one@users.noreply.github.com>
2022-01-11 05:37:36 +0600

    Update animate.py

--- Tom and Jerry in the terminal ASCII animation/animate.py
+++ Tom and Jerry in the terminal ASCII animation/animate.py
@@ -3,9 +3,8 @@ import pickle,time,os
 with open('data.pickle','rb') as fp:
 	frames=pickle.load(fp)
 
-for i in range(1):
-	for frame in frames:
-		print(''.join(frame))
-		time.sleep(.040)
-		os.system("clear")
-		#os.system("cls") #if you are in windows(command prompt)
+for frame in frames:
+	print(''.join(frame))
+	time.sleep(.040)
+	os.system("clear")
+	#os.system("cls") #if you are in windows(command prompt)
