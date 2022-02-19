9c5efa795e49fe6e15aaaf2ea52b249f10a40584
01one <84787013+01one@users.noreply.github.com>
2022-02-19 18:25:12 +0600

    Add files via upload

---
+++ physics/calculate_time_dilation.py
@@ -0,0 +1,14 @@
+from math import sqrt
+c=299792458 #speed of light in m/s
+
+t=eval(input("Enter the time passed on earth in year: "))
+v=c
+while not v<c:
+	v=eval(input("Enter the speed of the object in m/s: "))
+	if v>c:
+		print("Speed of the object must be less than the speed of light:",c)
+def time_dilation(c,v,t):
+	t_0=t*sqrt(1-(v**2/c**2))
+	return t_0
+print("time passed to the object",time_dilation(c,v,t)," year")
+		
